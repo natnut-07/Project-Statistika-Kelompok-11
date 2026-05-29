4d1560b4970bbf632c51fe6cde1b27e6ae9996bc
natnut-07 <natasyanurafriyani@gmail.com>
2026-05-29 10:27:20 -0700

    Fix Final

--- fetch_pandas_data.py
+++ fetch_pandas_data.py
@@ -3,11 +3,9 @@ import pandas as pd
 import time
 import os
 
-# --- KONFIGURASI ---
-TOKEN = "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" # ⚠️ SEGERA RESET TOKEN INI SETELAH BERHASIL!
+TOKEN = "Token_GITHUB_ANDA"  # Ganti dengan token GitHub Anda
 headers = {"Authorization": f"token {TOKEN}"}
 
-# PATH ABSOLUT
 BASE_DIR = r'C:\Users\lenovo\Desktop\GitHub\Stat\Project-Statistika-Kelompok-11'
 RAW_FOLDER = os.path.join(BASE_DIR, 'data', 'raw')
 os.makedirs(RAW_FOLDER, exist_ok=True)
@@ -30,11 +28,9 @@ def fetch_github_data(category="issues", total_pages=15):
             continue
     return pd.DataFrame(all_data)
 
-# Jalankan Penarikan
 df_iss_raw = fetch_github_data("issues", total_pages=60)
 df_prs_raw = fetch_github_data("pulls", total_pages=15)
 
-# SIMPAN HANYA DATA MENTAH
 df_iss_raw.to_csv(os.path.join(RAW_FOLDER, 'pandas_issues_raw.csv'), index=False)
 df_prs_raw.to_csv(os.path.join(RAW_FOLDER, 'pandas_prs_raw.csv'), index=False)
 
